a9ea405b3e67cefb0872621c14279691182796c9
北沢亮和 <ryowa.kitazawa@gmail.com>
2022-12-03 19:59:50 +0900

    メモ更新

--- memo/memo.kt
+++ memo/memo.kt
@@ -3,11 +3,11 @@ package memo
 class memo {
 
     /* 1つの数字 */
-    val n = readLine()!!.toInt()
+    val imputNum = readLine()!!.toInt()
 
     /* 1行に複数の数字(リストとして受け取る) ex:[1, 2, 3] */
-    val a = readLine()!!.split(" ").map{it.toInt()}
+    val imputNum2 = readLine()!!.split(" ").map{it.toInt()}
 
     /* 複数行の複数の数字をリストとして受け取る */
-    val maze = (0 until 3).map{readLine()!!.split(' ').map{it.toInt()}}
+    val imputNum3 = (0 until 3).map{readLine()!!.split(' ').map{it.toInt()}}
 }
