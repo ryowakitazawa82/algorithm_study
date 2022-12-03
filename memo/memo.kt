1bf79a2fb32092700bd6af04c36860620c0127e9
北沢亮和 <ryowa.kitazawa@gmail.com>
2022-12-03 21:41:46 +0900

    更新

--- memo/memo.kt
+++ memo/memo.kt
@@ -10,4 +10,9 @@ class memo {
 
     /* 複数行の複数の数字をリストとして受け取る */
     val imputNum3 = (0 until 3).map{readLine()!!.split(' ').map{it.toInt()}}
+
+    /* 複数行の複数の数字をリストとして受け取る（ 1行目の0番目のリストにn行ある事が入力されて、2行目以降に実際のデータがあるパターン） */
+    val firstNum = readLine()!!.split(" ").map{it.toInt()}
+    val afterFirstNum = (0 until firstNum[0]).map{readLine()!!.split(' ').map{it.toInt()}}
+    //imputNumを算出するとき、firstNumは引かれている！！
 }
